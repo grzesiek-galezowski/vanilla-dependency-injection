ea43bb4baa340d33ad2f2ea0f2128abc790be706
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2023-10-30 14:25:50 +0100

    Create MultipleLifetimesOfInstancesTheSameClass.cs

---
+++ DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/MultipleLifetimesOfInstancesTheSameClass.cs
@@ -0,0 +1,99 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiFrameworkCons;
+
+internal class MultipleLifetimesOfInstancesTheSameClass
+{
+  [Test]
+  public void ShouldComposeWithTwoLifestylesOfThrottledOutbox()
+  {
+    //GIVEN
+    var o = new ThrottledOutbox();
+    var p1 = new OnDemandProcess(o);
+    var p2 = new ScheduledProcess(o);
+    var p3 = new EmergencyProcess(
+      new ThrottledOutbox()
+    );
+
+    //THEN
+    Assert.AreSame(p2.ThrottledOutbox, p1.ThrottledOutbox);
+    Assert.AreNotSame(p2.ThrottledOutbox, p3.ThrottledOutbox);
+    Assert.AreNotSame(p1.ThrottledOutbox, p3.ThrottledOutbox);
+  }
+
+  [Test]
+  public void ShouldComposeWithTwoLifestylesOfThrottledOutbox_UsingMsDi()
+  {
+    //GIVEN
+    var services = new ServiceCollection();
+
+    services.AddKeyedSingleton<ThrottledOutbox>("shared");
+    services.AddTransient<ThrottledOutbox>();
+    services.AddTransient(ctx =>
+      ActivatorUtilities.CreateInstance<OnDemandProcess>(
+        ctx,
+        ctx.GetRequiredKeyedService<ThrottledOutbox>("shared")));
+    services.AddTransient(ctx =>
+      ActivatorUtilities.CreateInstance<ScheduledProcess>(
+        ctx,
+        ctx.GetRequiredKeyedService<ThrottledOutbox>("shared")));
+    services.AddTransient<EmergencyProcess>();
+
+    using var container = services.BuildServiceProvider(new ServiceProviderOptions()
+    {
+      ValidateOnBuild = true,
+      ValidateScopes = true
+    });
+
+    //WHEN
+    var p1 = container.GetRequiredService<OnDemandProcess>();
+    var p2 = container.GetRequiredService<ScheduledProcess>();
+    var p3 = container.GetRequiredService<EmergencyProcess>();
+
+
+    //THEN
+    Assert.AreSame(p2.ThrottledOutbox, p1.ThrottledOutbox);
+    Assert.AreNotSame(p2.ThrottledOutbox, p3.ThrottledOutbox);
+    Assert.AreNotSame(p1.ThrottledOutbox, p3.ThrottledOutbox);
+  }
+
+  [Test]
+  public void ShouldComposeWithTwoLifestylesOfThrottledOutbox_UsingAutofac()
+  {
+    //GIVEN
+    var builder = new ContainerBuilder();
+
+    builder.RegisterType<ThrottledOutbox>()
+      .Keyed<ThrottledOutbox>("shared")
+      .SingleInstance();
+    builder.RegisterType<ThrottledOutbox>().InstancePerLifetimeScope();
+    builder.RegisterType<OnDemandProcess>().WithParameter(
+      (info, _) => info.Position == 0,
+      (_, context) => context.ResolveKeyed<ThrottledOutbox>("shared"));
+    builder.RegisterType<ScheduledProcess>()
+      .WithParameter(
+        (info, _) => info.Position == 0,
+        (_, context) => context.ResolveKeyed<ThrottledOutbox>("shared"));
+    builder.RegisterType<EmergencyProcess>().InstancePerLifetimeScope();
+
+    using var container = builder.Build();
+
+    //WHEN
+    var p1 = container.Resolve<OnDemandProcess>();
+    var p2 = container.Resolve<ScheduledProcess>();
+    var p3 = container.Resolve<EmergencyProcess>();
+
+    //THEN
+    Assert.AreSame(p2.ThrottledOutbox, p1.ThrottledOutbox);
+    Assert.AreNotSame(p2.ThrottledOutbox, p3.ThrottledOutbox);
+    Assert.AreNotSame(p1.ThrottledOutbox, p3.ThrottledOutbox);
+  }
+}
+
+internal record EmergencyProcess(ThrottledOutbox ThrottledOutbox);
+internal record ScheduledProcess(ThrottledOutbox ThrottledOutbox);
+internal record OnDemandProcess(ThrottledOutbox ThrottledOutbox);
+
+internal class ThrottledOutbox
+{
+}
