92f77b98cc26f4de58ce7545c8534b4e0ddf2fa6
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2023-12-04 23:51:00 +0100

    Update Decorators2.cs

--- DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/Decorators2.cs
+++ DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/Decorators2.cs
@@ -24,6 +24,39 @@ public class Decorators2
     Assert.IsInstanceOf<D>(chain2.Next.Next!.Next);
     Assert.IsNull(chain2.Next.Next!.Next!.Next);
   }
+  
+  [Test]
+  public void ShouldComposeVariousDecoratorConfigurationsWithMsDi()
+  {
+    //GIVEN
+    var services = new ServiceCollection();
+    services.AddKeyedSingleton("chain1",
+      (c, _) => ActivatorUtilities.CreateInstance<A>(c,
+        ActivatorUtilities.CreateInstance<B>(c,
+          ActivatorUtilities.CreateInstance<C1>(c,
+            ActivatorUtilities.CreateInstance<D>(c)
+            ))));
+    services.AddKeyedSingleton("chain2",
+      (c, _) => ActivatorUtilities.CreateInstance<A>(c,
+        ActivatorUtilities.CreateInstance<B>(c,
+          ActivatorUtilities.CreateInstance<C2>(c,
+            ActivatorUtilities.CreateInstance<D>(c)))));
+
+    //WHEN
+    using var container = services.BuildServiceProvider();
+    var chain1 = container.GetRequiredKeyedService<A>("chain1");
+    var chain2 = container.GetRequiredKeyedService<A>("chain2");
+
+    //THEN
+    Assert.IsInstanceOf<B>(chain1.Next);
+    Assert.IsInstanceOf<C1>(chain1.Next.Next);
+    Assert.IsInstanceOf<D>(chain1.Next.Next!.Next);
+
+    Assert.IsInstanceOf<B>(chain2.Next);
+    Assert.IsInstanceOf<C2>(chain2.Next.Next);
+    Assert.IsInstanceOf<D>(chain2.Next.Next!.Next);
+    Assert.IsNull(chain2.Next.Next!.Next!.Next);
+  }
 
 
   public interface IComponent
