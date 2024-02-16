47637be471ae0259173cc421d1ec7e9b86160b50
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2024-02-16 20:15:53 +0100

    Update DependencyAsMultipleInterfaces.cs

--- DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/DependencyAsMultipleInterfaces.cs
+++ DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/DependencyAsMultipleInterfaces.cs
@@ -1,26 +1,62 @@
 namespace DiFrameworkCons;
 
+//todo add description
 public class DependencyAsMultipleInterfaces
 {
   [Test]
-  public void ShouldRegisterSingleInstancesWhenRegisteringSingleTypeAsTwoInterfaces()
+  public void ShouldUseOneInstanceForDifferentInterfacesUsingVanillaDi()
+  {
+    //GIVEN
+    var cache = new Cache();
+
+    //WHEN
+    var cacheUser = new UserOfReaderAndWriter(cache, cache);
+
+    //THEN
+    Assert.AreSame(cacheUser.ReadCache, cacheUser.WriteCache);
+    Assert.AreEqual(cacheUser.ReadCache.Number, cacheUser.WriteCache.Number);
+  }
+
+  [Test]
+  public void ShouldRegisterSingleInstancesWhenRegisteringSingleTypeAsTwoInterfacesUsingAutofac()
   {
     //GIVEN
     var containerBuilder = new ContainerBuilder();
+    containerBuilder.RegisterType<UserOfReaderAndWriter>().SingleInstance();
     containerBuilder.RegisterType<Cache>()
       .SingleInstance()
       .AsImplementedInterfaces();
 
     using var container = containerBuilder.Build();
     //WHEN
-    var readCache = container.Resolve<IReadCache>();
-    var writeCache = container.Resolve<IWriteCache>();
+    var cacheUser = container.Resolve<UserOfReaderAndWriter>();
 
     //THEN
-    Assert.AreEqual(readCache.Number, writeCache.Number);
-    Assert.AreSame(readCache, writeCache);
+    Assert.AreSame(cacheUser.ReadCache, cacheUser.WriteCache);
+    Assert.AreEqual(cacheUser.ReadCache.Number, cacheUser.WriteCache.Number);
+  }
+
+  [Test]
+  public void ShouldRegisterSingleInstancesWhenRegisteringSingleTypeAsTwoInterfacesUsingMsDi()
+  {
+    //GIVEN
+    var builder = new ServiceCollection();
+    builder.AddSingleton<UserOfReaderAndWriter>();
+    builder.AddSingleton<Cache>();
+    builder.AddSingleton<IReadCache>(c => c.GetRequiredService<Cache>());
+    builder.AddSingleton<IWriteCache>(c => c.GetRequiredService<Cache>());
+
+    using var container = builder.BuildServiceProvider();
+    //WHEN
+    var cacheUser = container.GetRequiredService<UserOfReaderAndWriter>();
+
+    //THEN
+    Assert.AreSame(cacheUser.ReadCache, cacheUser.WriteCache);
+    Assert.AreEqual(cacheUser.ReadCache.Number, cacheUser.WriteCache.Number);
   }
 }
+
+public record UserOfReaderAndWriter(IReadCache ReadCache, IWriteCache WriteCache);
 
 public interface IWriteCache
 {
