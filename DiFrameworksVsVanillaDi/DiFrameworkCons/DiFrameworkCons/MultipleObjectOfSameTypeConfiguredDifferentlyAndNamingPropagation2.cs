43ccefd1da6f8a24e079d29466f1ae1eb1b8ff75
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2024-04-17 22:47:12 +0200

    Update MultipleObjectOfSameTypeConfiguredDifferentlyAndNamingPropagation2.cs

--- DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/MultipleObjectOfSameTypeConfiguredDifferentlyAndNamingPropagation2.cs
+++ DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/MultipleObjectOfSameTypeConfiguredDifferentlyAndNamingPropagation2.cs
@@ -162,6 +162,41 @@ class MultipleObjectOfSameTypeConfiguredDifferentlyAndNamingPropagation2
   }
 
   [Test]
+  public void ShouldResolveTwoSimilarObjectGraphsWithDifferentLeavesFromMsDiAndActivatorUtilities()
+  {
+    //GIVEN
+    var builder = new ServiceCollection();
+
+    //Works only if created objects are transients
+    //and does not allow reusing the nested instances created by ActivatorUtilities
+    builder.AddSingleton(c => ActivatorUtilities.CreateInstance<World>(c,
+      ActivatorUtilities.CreateInstance<Character>(c,
+        ActivatorUtilities.CreateInstance<Armor>(c,
+          ActivatorUtilities.CreateInstance<ChainMail>(c)),
+        ActivatorUtilities.CreateInstance<LongSword>(c)),
+      ActivatorUtilities.CreateInstance<Character>(c,
+        ActivatorUtilities.CreateInstance<Armor>(c,
+          ActivatorUtilities.CreateInstance<BreastPlate>(c)),
+        ActivatorUtilities.CreateInstance<ShortSword>(c))));
+    builder.AddTransient<Helmet>();
+
+    using var container = builder.BuildServiceProvider();
+
+    //WHEN
+    var world = container.GetRequiredService<World>();
+
+    //THEN
+    world.Enemy.Should().NotBeSameAs(world.Hero);
+    world.Enemy.Armor.Should().NotBeSameAs(world.Hero.Armor);
+    world.Enemy.Armor.Helmet.Should().NotBeSameAs(world.Hero.Armor.Helmet);
+
+    world.Hero.Armor.BodyArmor.Should().BeOfType<ChainMail>();
+    world.Enemy.Armor.BodyArmor.Should().BeOfType<BreastPlate>();
+    world.Hero.Weapon.Should().BeOfType<LongSword>();
+    world.Enemy.Weapon.Should().BeOfType<ShortSword>();
+  }
+
+  [Test]
   public void ShouldResolveTwoSimilarObjectGraphsWithDifferentLeavesFromContainerModulesUsingMsDi()
   {
     //GIVEN
@@ -170,6 +205,7 @@ class MultipleObjectOfSameTypeConfiguredDifferentlyAndNamingPropagation2
       c,
       c.GetRequiredKeyedService<Character>("hero"),
       c.GetRequiredKeyedService<Character>("enemy")));
+
     SoldierMsDiModule<LongSword, ChainMail>.RegisterIn(builder, "hero");
     SoldierMsDiModule<ShortSword, BreastPlate>.RegisterIn(builder, "enemy");
 
