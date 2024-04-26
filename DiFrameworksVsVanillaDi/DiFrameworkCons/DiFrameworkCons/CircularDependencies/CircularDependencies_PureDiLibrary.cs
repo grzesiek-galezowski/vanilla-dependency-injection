9087bb9a228f656fc0e2b93336278db2af3b5401
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2024-04-26 13:32:02 +0200

    wip

---
+++ DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/CircularDependencies/CircularDependencies_PureDiLibrary.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework.Internal;
+using Pure.DI;
+
+namespace DiFrameworkCons.CircularDependencies;
+
+public static class CircularDependencies_PureDiLibrary
+{
+  /// <summary>
+  /// Pure.DI impressively shows the circular dependency at
+  /// the time of compilation. Just uncomment the commented
+  /// out line to see the compile error.
+  /// </summary>
+  [Test]
+  public static void ShouldShowFailureWhenCircularDependencyIsDiscoveredWithPureDiLibrary()
+  {
+    DI.Setup($"{nameof(CircularDependencies_PureDiLibrary)}CompositionRoot")
+      //uncomment to show compile error: .Root<One>()
+      ;
+
+    _ = new CircularDependencies_PureDiLibraryCompositionRoot();
+  }
+}
