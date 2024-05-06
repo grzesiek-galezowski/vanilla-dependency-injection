7af2e158f38768689de92d6dcaec9ebb5eb69928
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2024-05-07 00:12:11 +0200

    Update DeadCode_PureDiLibrary.cs

--- DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/CompileTimeVsRuntime/DeadCode/DeadCode_PureDiLibrary.cs
+++ DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/CompileTimeVsRuntime/DeadCode/DeadCode_PureDiLibrary.cs
@@ -2,11 +2,13 @@ using Pure.DI;
 
 namespace DiFrameworkCons.CompileTimeVsRuntime.DeadCode;
 
+/// <summary>
+/// Even though Pure DI is a code generator,
+/// the IDE does not show which parts of the generated code
+/// are not used...
+/// </summary>
 public class DeadCode_PureDiLibrary
 {
-  /// <summary>
-  /// BUG: fill
-  /// </summary>
   [Test]
   public static void ContainerContainsSomeDeadCodeWithMsDi()
   {
@@ -29,10 +31,12 @@ partial class Composition4
     DI.Setup(nameof(Composition4))
       .Bind().As(Lifetime.Singleton).To<Dependency>()
       .Bind<DependencyConsumer>()
-        .As(Lifetime.Singleton)
-        .To<DependencyConsumer>()
-        .Root<DependencyConsumer>("Consumer")
+      .As(Lifetime.Singleton)
+      .To<DependencyConsumer>()
       //dead code
-      .Bind().As(Lifetime.Singleton).To<DeadCode_VanillaDi>();
+      .Bind().As(Lifetime.Singleton).To<DeadCode_VanillaDi>()
+      .Root<Dependency>("Dependency")
+      //end dead code;
+      .Root<DependencyConsumer>("Consumer");
   }
 }
