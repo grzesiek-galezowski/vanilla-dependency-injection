9684f954f4aea4de3e2121c539e70ace40acd773
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2024-05-11 22:28:40 +0200

    Added more examples to decorator section

--- DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/MultipleRecipes/Decorators/_2_WithMultipleChains/DecoratorsWithMultipleChains_Lamar.cs
+++ DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/MultipleRecipes/Decorators/_2_WithMultipleChains/DecoratorsWithMultipleChains_Lamar.cs
@@ -42,5 +42,49 @@ public static class DecoratorsWithMultipleChains_Lamar
     chain2.Next.Next!.Next!.Next.Should().BeNull();
   }
 
-  //TODO: other examples using Lamar unique features.
+  /// <summary>
+  /// An alternative is to use keyed dependencies.
+  /// Note that starting from the difference, the requirement
+  /// to use keys propagates.
+  /// </summary>
+  [Test]
+  public static void ShouldComposeVariousDecoratorConfigurationsWithKeyedDependencies()
+  {
+    //GIVEN
+    var chain1Key = "chain1";
+    var chain2Key = "chain2";
+    var container = new Container(services =>
+    {
+      services.AddTransient<IComponent, D>();
+      services.AddTransient<C1>();
+      services.AddTransient<C2>();
+      services.AddKeyedTransient<B>(chain1Key,
+        (x, key) => ActivatorUtilities.CreateInstance<B>(
+          x, x.GetRequiredService<C1>()));
+      services.AddKeyedTransient<B>(chain2Key,
+        (x, key) => ActivatorUtilities.CreateInstance<B>(
+          x, x.GetRequiredService<C2>()));
+      services.AddKeyedSingleton<A>(chain1Key,
+        (x, key) => ActivatorUtilities.CreateInstance<A>(
+          x, x.GetRequiredKeyedService<B>(key)));
+      services.AddKeyedSingleton<A>(chain2Key,
+        (x, key) => ActivatorUtilities.CreateInstance<A>(
+          x, x.GetRequiredKeyedService<B>(key)));
+    });
+
+    //WHEN
+    var chain1 = container.GetRequiredKeyedService<A>(chain1Key);
+    var chain2 = container.GetRequiredKeyedService<A>(chain2Key);
+
+    //THEN
+    chain1.Next.Should().BeOfType<B>();
+    chain1.Next.Next.Should().BeOfType<C1>();
+    chain1.Next.Next!.Next.Should().BeOfType<D>();
+
+    chain2.Next.Should().BeOfType<B>();
+    chain2.Next.Next.Should().BeOfType<C2>();
+    chain2.Next.Next!.Next.Should().BeOfType<D>();
+    chain2.Next.Next!.Next!.Next.Should().BeNull();
+  }
+
 }
