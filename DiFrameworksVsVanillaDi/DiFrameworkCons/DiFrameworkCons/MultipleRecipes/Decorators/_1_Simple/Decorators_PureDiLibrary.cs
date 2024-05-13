d71e0d48921ffc65274f2140047c5520167289f5
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2024-05-13 22:15:32 +0200

    Update Decorators_PureDiLibrary.cs

--- DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/MultipleRecipes/Decorators/_1_Simple/Decorators_PureDiLibrary.cs
+++ DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/MultipleRecipes/Decorators/_1_Simple/Decorators_PureDiLibrary.cs
@@ -4,29 +4,16 @@ namespace DiFrameworkCons.MultipleRecipes.Decorators._1_Simple;
 
 public class Decorators_PureDiLibrary
 {
-  /// <summary>
-  /// Pure.DI allows compositions by tagging parameters
-  /// in constructors. This requires coupling classes
-  /// to the composition mechanism and the generator
-  /// can bind only one type to tag.
-  ///
-  /// In this example, I used subclasses and tagged them
-  /// instead of the "production" classes. Normally
-  /// that's not what I believe I should be doing,
-  /// but here I wanted to keep the shared classes
-  /// independent of Pure.DI.
-  ///
-  /// Still, it works for simple cases.
-  /// </summary>
+
   [Test]
   public static void ContainerContainsSomeDeadCodeWithMsDi()
   {
     //GIVEN
-    var composition = new Composition7(X: 1);
+    var composition = new Composition7();
 
     var answer = composition.Answer;
-    answer.Should().BeOfType<TaggedSynchronizedAnswer>();
-    answer.NestedAnswer.Should().BeOfType<TaggedTracedAnswer>();
+    answer.Should().BeOfType<SynchronizedAnswer>();
+    answer.NestedAnswer.Should().BeOfType<TracedAnswer>();
     answer.NestedAnswer.NestedAnswer.Should().BeOfType<Answer>();
     ((SynchronizedAnswer)answer).X.Should().Be(1);
   }
@@ -37,16 +24,19 @@ partial class Composition7
   public void Setup()
   {
     DI.Setup(nameof(Composition7))
-      .Bind("tracedNested").To<Answer>()
-      .Bind("synchronizedNested").To<TaggedTracedAnswer>()
-      .RootBind<IAnswer>("Answer").To<TaggedSynchronizedAnswer>().Arg<int>("X");
+      .Bind<IAnswer>("answer").To<Answer>()
+      .Bind<IAnswer>("tracedAnswer").To(
+        context =>
+        {
+          context.Inject<IAnswer>("answer", out var answer);
+          return new TracedAnswer(answer);
+        })
+      .Bind<IAnswer>("synchronizedAnswer").To(
+        context =>
+        {
+          context.Inject<IAnswer>("tracedAnswer", out var answer);
+          return new SynchronizedAnswer(answer, 1);
+        })
+      .Root<IAnswer>("Answer", "synchronizedAnswer");
   }
 }
-
-public record TaggedTracedAnswer(
-  [Tag("tracedNested")] IAnswer NestedAnswer)
-  : TracedAnswer(NestedAnswer);
-
-public record TaggedSynchronizedAnswer(
-  [Tag("synchronizedNested")] IAnswer NestedAnswer, int X)
-  : SynchronizedAnswer(NestedAnswer, X);
