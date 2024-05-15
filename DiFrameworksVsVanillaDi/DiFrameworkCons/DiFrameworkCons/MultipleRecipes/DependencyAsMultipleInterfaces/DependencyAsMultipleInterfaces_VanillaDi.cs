f289746def81f1b4731b4badb2154c5113c3fc6d
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2024-05-15 21:54:42 +0200

    Update DependencyAsMultipleInterfaces_VanillaDi.cs

--- DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/MultipleRecipes/DependencyAsMultipleInterfaces/DependencyAsMultipleInterfaces_VanillaDi.cs
+++ DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/MultipleRecipes/DependencyAsMultipleInterfaces/DependencyAsMultipleInterfaces_VanillaDi.cs
@@ -1,3 +1,5 @@
+using Pure.DI;
+
 namespace DiFrameworkCons.MultipleRecipes.DependencyAsMultipleInterfaces;
 
 public class DependencyAsMultipleInterfaces_VanillaDi
@@ -11,14 +13,39 @@ public class DependencyAsMultipleInterfaces_VanillaDi
   public void ShouldUseOneInstanceForDifferentInterfacesUsingVanillaDi()
   {
     //GIVEN
-    var cache = new Cache();
+    var composition = new Composition9();
 
     //WHEN
-    var cacheUser = new UserOfReaderAndWriter(cache, cache);
+    var cacheUser = composition.Root;
 
     //THEN
     cacheUser.WriteCache.Should().BeSameAs(cacheUser.ReadCache);
     cacheUser.WriteCache.Number.Should().Be(cacheUser.ReadCache.Number);
+  }
+}
+
+public partial class Composition9
+{
+  public void Setup()
+  {
+    DI.Setup(nameof(Composition9))
+      .Bind<Cache>().As(Lifetime.Singleton).To<Cache>()
+      .Bind<IReadCache>().As(Lifetime.Singleton).To(
+        context =>
+        {
+          //tempting to extract a helper method
+          //but this breaks code generation...
+          context.Inject<Cache>(out var cache);
+          return cache;
+        })
+      .Bind<IWriteCache>().As(Lifetime.Singleton).To(
+        context =>
+        {
+          context.Inject<Cache>(out var cache);
+          return cache;
+        })
+      .Bind<UserOfReaderAndWriter>().To<UserOfReaderAndWriter>()
+      .Root<UserOfReaderAndWriter>("Root");
   }
 }
 
