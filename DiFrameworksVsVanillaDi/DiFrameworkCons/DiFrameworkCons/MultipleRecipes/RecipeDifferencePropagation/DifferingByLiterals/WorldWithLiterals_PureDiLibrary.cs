567b4ad4737489ccea35795feb34077fa9d25082
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2024-05-25 18:08:01 +0200

    wip

---
+++ DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/MultipleRecipes/RecipeDifferencePropagation/DifferingByLiterals/WorldWithLiterals_PureDiLibrary.cs
@@ -0,0 +1,79 @@
+using Pure.DI;
+
+namespace DiFrameworkCons.MultipleRecipes.RecipeDifferencePropagation.DifferingByLiterals;
+
+static class WorldWithLiterals_PureDiLibrary
+{
+  [Test]
+  public static void ShouldResolveTwoSimilarObjectGraphsWithDifferentLeavesFromVanillaDi()
+  {
+    //GIVEN
+    var composition = new Composition15();
+
+    //WHEN
+    var world = composition.World;
+
+    world.Enemy.Should().NotBeSameAs(world.Hero);
+    world.Enemy.Armor.Should().NotBeSameAs(world.Hero.Armor);
+    world.Enemy.Armor.Helmet.Should().NotBeSameAs(world.Hero.Armor.Helmet);
+    world.Enemy.Armor.BreastPlate.Should().NotBeSameAs(world.Hero.Armor.BreastPlate);
+    world.Enemy.Armor.BreastPlate.Defense.Should().NotBe(world.Hero.Armor.BreastPlate.Defense);
+    world.Enemy.Sword.Should().NotBeSameAs(world.Hero.Sword);
+
+    world.Hero.Sword.Attack.Should().Be(4);
+    world.Hero.Armor.BreastPlate.Defense.Should().Be(2);
+    world.Enemy.Sword.Attack.Should().Be(6);
+    world.Enemy.Armor.BreastPlate.Defense.Should().Be(4);
+  }
+}
+
+partial class Composition15
+{
+  public void Setup()
+  {
+    DI.Setup(nameof(Composition15))
+      .RootBind<World>("World").As(Lifetime.Singleton).To(context =>
+      {
+        context.Inject<Character>("first", out var hero);
+        context.Inject<Character>("second", out var enemy);
+        return new World(hero, enemy);
+      })
+      .Bind<Character>("first").As(Lifetime.Singleton).To(context =>
+      {
+        context.Inject<Armor>("first", out var armor);
+        context.Inject<Sword>("first", out var sword);
+        return new Character(armor, sword);
+      })
+      .Bind<Character>("second").As(Lifetime.Singleton).To(context =>
+      {
+        context.Inject<Armor>("second", out var armor);
+        context.Inject<Sword>("second", out var sword);
+        return new Character(armor, sword);
+      })
+      .Bind<Helmet>().As(Lifetime.Transient).To<Helmet>()
+      .Bind<Armor>("first").As(Lifetime.Singleton).To(context =>
+      {
+        context.Inject<Helmet>(out var helmet);
+        context.Inject<BreastPlate>("first", out var breastPlate);
+        return new Armor(helmet, breastPlate);
+      })
+      .Bind<Armor>("second").As(Lifetime.Singleton).To(context =>
+      {
+        context.Inject<Helmet>(out var helmet);
+        context.Inject<BreastPlate>("second", out var breastPlate);
+        return new Armor(helmet, breastPlate);
+      })
+      .Bind<BreastPlate>("first")
+      .As(Lifetime.Singleton)
+      .To(_ => new BreastPlate(2))
+      .Bind<BreastPlate>("second")
+      .As(Lifetime.Singleton)
+      .To(_ => new BreastPlate(4))
+      .Bind<Sword>("first")
+      .As(Lifetime.Singleton)
+      .To(_ => new Sword(4))
+      .Bind<Sword>("second")
+      .As(Lifetime.Singleton)
+      .To(_ => new Sword(6));
+  }
+}
---
+++ DiFrameworksVsVanillaDi/DiFrameworkCons/DiFrameworkCons/MultipleRecipes/RecipeDifferencePropagation/DifferingByLiterals/WorldWithLiterals_SimpleInjector.cs
@@ -0,0 +1,49 @@
+using SimpleInjector;
+
+namespace DiFrameworkCons.MultipleRecipes.RecipeDifferencePropagation.DifferingByLiterals;
+
+class WorldWithLiterals_SimpleInjector
+{
+  /// <summary>
+  /// I found no appealing way to use SimpleInjector's conditional registration.
+  ///
+  /// The only way I could find was to use an ActivatorUtilities.CreateInstance
+  /// </summary>
+  [Test]
+  public static void ShouldResolveTwoSimilarObjectGraphsWithDifferentLeavesUsingActivatorUtilities()
+  {
+    //GIVEN
+    using var container = new Container();
+    container
+      .Register(() =>
+        ActivatorUtilities.CreateInstance<World>(container,
+          CreateCharacter(container, breastPlateDefense: 2, swordAttack: 4),
+          CreateCharacter(container, breastPlateDefense: 4, swordAttack: 6)),
+        Lifestyle.Singleton);
+
+    //WHEN
+    var world = container.GetRequiredService<World>();
+
+    //THEN
+    world.Enemy.Should().NotBeSameAs(world.Hero);
+    world.Enemy.Armor.Should().NotBeSameAs(world.Hero.Armor);
+    world.Enemy.Armor.Helmet.Should().NotBeSameAs(world.Hero.Armor.Helmet);
+    world.Enemy.Armor.BreastPlate.Should().NotBeSameAs(world.Hero.Armor.BreastPlate);
+    world.Enemy.Armor.BreastPlate.Defense.Should().NotBe(world.Hero.Armor.BreastPlate.Defense);
+    world.Enemy.Sword.Should().NotBeSameAs(world.Hero.Sword);
+
+    world.Hero.Sword.Attack.Should().Be(4);
+    world.Hero.Armor.BreastPlate.Defense.Should().Be(2);
+    world.Enemy.Sword.Attack.Should().Be(6);
+    world.Enemy.Armor.BreastPlate.Defense.Should().Be(4);
+  }
+
+  private static Character CreateCharacter(IServiceProvider x, int breastPlateDefense, int swordAttack)
+  {
+    return ActivatorUtilities.CreateInstance<Character>(x,
+      ActivatorUtilities.CreateInstance<Armor>(x,
+        ActivatorUtilities.CreateInstance<Helmet>(x),
+        ActivatorUtilities.CreateInstance<BreastPlate>(x, breastPlateDefense)),
+      ActivatorUtilities.CreateInstance<Sword>(x, swordAttack));
+  }
+}
